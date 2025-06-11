e0c00d35dd7f1eaae2bf020cd5b5c63bf0ec23ec
ModitTdr <modittuladhar@gmail.com>
2025-06-11 15:58:29 +0545

    stop nav to authpg after login

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import { useEffect } from 'react'
-import { Outlet } from "react-router";
+import { Link, Outlet } from "react-router";
 import { jwtDecode } from "jwt-decode";
 import { useDispatch } from 'react-redux';
 import axios from 'axios';
@@ -9,8 +9,6 @@ import { restoreLogin } from './store/features/auth/authSlice';
 
 function App() {
   const dispatch = useDispatch();
-
-
   useEffect(() => {
     const token = localStorage.getItem("token");
     if (token) {
@@ -24,7 +22,7 @@ function App() {
   return (
     <div className='bg-background text-text smooth-transition p-4 lg:px-8 py-4 h-screen flex flex-col'>
       <header className='flex justify-between items-center flex-wrap w-full container m-auto'>
-        <h1 className="text-3xl font-[Montserrat]">Room Finder</h1>
+        <Link to='/' className="text-3xl font-[Montserrat]">Room Finder</Link>
         <Navbar />
       </header>
       <div className='container m-auto my-8 flex-grow'>
