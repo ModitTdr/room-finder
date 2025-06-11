a088cee2d5b1acff0e700c1fdcb62802fffc39d3
ModitTdr <modittuladhar@gmail.com>
2025-06-11 15:48:34 +0545

    redux& api improv

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -23,9 +23,7 @@ export const getUserById = async (req, res) => {
     const user = await userService.getUserByIdService(userId);
     if (!user) return res.status(404).json({ message: "User Not Found"});
     const {password,created_at,updated_at, ...withoutPassword} = user
-    res.status(200).json({
-      data:withoutPassword
-    });
+    res.status(200).json({data:withoutPassword});
   }catch(error){
     console.error('Error fetching user:', error);
     res.status(500).json({ message: 'Internal server error' });
